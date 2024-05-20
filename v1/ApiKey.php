bd569b6e04af55871786b96ec8e3047435d7c75e
Eugene Panin <varenich@gmail.com>
2024-05-20 10:41:23 +0300

    Comments added, translated to english

--- v1/ApiKey.php
+++ v1/ApiKey.php
@@ -5,7 +5,12 @@ class ApiKey {
     private $_configPath;
     private $_keys;
 
-    public function __construct($configPath) {
+    /*
+    Creates an object, loads keys from JSON file located at configPath
+
+    configPath : string - Path to JSON file of API keys
+    */
+    public function __construct($configPath='') {
         $this->_configPath = $configPath;
 
         $jsonconfigstr = file_get_contents($configPath);
@@ -14,18 +19,22 @@ class ApiKey {
         $this->_keys = @$jsonconfig["apiKeys"];
     } // __construct
 
-    public function check($restrictionName,$keyToCheck) {
-        //print_r($this->_keys);
-        //echo "#####";
+    /*
+    Checks if keyToCheck exists in keys array of a given restriction
+
+    restrictionName : string - key of an api keys array related to restriction (loaded from configPath)
+    keyToCheck : string - API key to check
+    */
+    public function check($restrictionName='',$keyToCheck='') {
         if (@$this->_keys[$restrictionName]) {
             $res = array();
             foreach ($this->_keys[$restrictionName] as $key=>$val) {
                 $res = array_merge( $res, array_values($val) );
             }
             if (in_array($keyToCheck, $res) ) return true;
-            throw new \Exception('Доступ запрещен', 403);
+            throw new \Exception('Access denied', 403);
         }
-        throw new \Exception('Хранилище ключей пустое');
+        throw new \Exception('Key storege is empty. Check the path to config');
     } // check
 } // class
 ?>
